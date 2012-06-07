7ebadceb99442e84142805449cbccd1cccb9c59e
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2012-06-07 15:50:27 +0200

    Use new ModelTestCase functionality.

--- test/src/it/polimi/models/PolimiTest.java
+++ test/src/it/polimi/models/PolimiTest.java
@@ -4,24 +4,15 @@ import android.content.res.AssetManager;
 import android.test.InstrumentationTestCase;
 import com.androidrecord.ActiveRecordBase;
 import com.androidrecord.migrations.Migrations;
+import com.androidrecord.test.ModelTestCase;
 
 import java.util.Arrays;
 import java.util.List;
 
-public abstract class PolimiTest<T extends ActiveRecordBase> extends InstrumentationTestCase {
-    protected Migrations migrations;
-
-    protected void setUp() throws Exception {
-        super.setUp();
-        AssetManager applicationAssets = getInstrumentation().getTargetContext().getAssets();
-        migrations = new Migrations(applicationAssets);
-    }
+public abstract class PolimiTest<T extends ActiveRecordBase> extends ModelTestCase<T> {
 
     public void testFirstMigrationCreatesDatabaseTable() throws Exception {
-        List<String> migrationStatements = Arrays.asList(migrations.loadMigrationNumber(1));
-        String createTableSql = ActiveRecordBase.createSqlFor(getClassUnderTest()).replace(";", "");
-
-        assertTrue("Create SQL missing. First migration doesn't contain: '" + createTableSql + "'", migrationStatements.contains(createTableSql));
+        assertDatabaseTableCreated(getClassUnderTest(), 1);
     }
 
     protected abstract Class<T> getClassUnderTest();
